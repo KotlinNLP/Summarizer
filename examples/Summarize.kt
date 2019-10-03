b48ab0e49584f80af4720e0a95b08b30cfb10c09
KotlinNLP Authors <github@kotlinnlp.com>
2019-10-03 22:04:26 +0200

    Add initial files

---
+++ examples/Summarize.kt
@@ -0,0 +1,181 @@
+/* Copyright 2019-present The KotlinNLP Authors. All Rights Reserved.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * ------------------------------------------------------------------*/
+
+import com.kotlinnlp.linguisticdescription.sentence.MorphoSynSentence
+import com.kotlinnlp.linguisticdescription.sentence.RealSentence
+import com.kotlinnlp.linguisticdescription.sentence.token.RealToken
+import com.kotlinnlp.morphologicalanalyzer.MorphologicalAnalyzer
+import com.kotlinnlp.morphologicalanalyzer.dictionary.MorphologyDictionary
+import com.kotlinnlp.neuralparser.helpers.labelerselector.MorphoSelector
+import com.kotlinnlp.neuralparser.language.ParsingSentence
+import com.kotlinnlp.neuralparser.language.ParsingToken
+import com.kotlinnlp.neuralparser.parsers.lhrparser.LHRModel
+import com.kotlinnlp.neuralparser.parsers.lhrparser.LHRParser
+import com.kotlinnlp.neuraltokenizer.Sentence as TkSentence
+import com.kotlinnlp.neuraltokenizer.NeuralTokenizer
+import com.kotlinnlp.neuraltokenizer.NeuralTokenizerModel
+import com.kotlinnlp.summarizer.Summarizer
+import com.kotlinnlp.utils.Timer
+import com.kotlinnlp.utils.progressindicator.ProgressIndicatorBar
+import com.xenomachina.argparser.mainBody
+import java.io.File
+import java.io.FileInputStream
+
+/**
+ * Summarize the text contained in a given file.
+ *
+ * Launch with the '-h' option for help about the command line arguments.
+ */
+fun main(args: Array<String>) = mainBody {
+
+  val helper = SummaryHelper(parsedArgs = CommandLineArguments(args))
+
+  while (true) {
+
+    println()
+    val filePath: String = readPath()
+
+    if (filePath.isEmpty()) {
+
+      break
+
+    } else {
+
+      val inputText: String = File(filePath).readText()
+
+      val summarizedSentences: List<Pair<TkSentence, Double>> = helper.summarize(inputText)
+      val scoredSentences: List<Pair<String, Double>> = summarizedSentences.map { it.first.buildText() to it.second }
+
+      while (true) {
+        println()
+        readSummaryStrength()?.also { printSummary(scoredSentences = scoredSentences, summaryStrength = it) } ?: break
+      }
+    }
+  }
+
+  println("\nExiting...")
+}
+
+/**
+ * Read the path of a textual file from the standard input.
+ *
+ * @return the path read
+ */
+private fun readPath(): String {
+
+  print("Insert the path of the file whose content will be summarized (empty to exit): ")
+
+  return readLine()!!
+}
+
+/**
+ * Read the summary strength from the standard input.
+ *
+ * @return the summary strength or null if the input was empty
+ */
+private fun readSummaryStrength(): Double? {
+
+  print("Insert the summary strength [0.0 - 1.0] (empty to skip to the next file): ")
+
+  return readLine()!!.let { if (it.isNotEmpty()) it.toDouble() else null }
+}
+
+/**
+ * Print a summary of the sentences with a given strength.
+ *
+ * @param scoredSentences the list of sentences with the related salience score
+ * @param summaryStrength the percentage of summary (0 means no summary) based on which
+ */
+private fun printSummary(scoredSentences: List<Pair<String, Double>>, summaryStrength: Double) {
+
+  val summary: String = scoredSentences
+    .asSequence()
+    .filter { it.second >= summaryStrength }
+    .map { it.first }
+    .joinToString("\n------------------------------------------\n")
+
+  println()
+  println("Summary:")
+  println(summary)
+}
+
+/**
+ * A helper that summarizes a text and prints progress information.
+ *
+ * @param parsedArgs the parsed command line arguments
+ */
+private class SummaryHelper(parsedArgs: CommandLineArguments) {
+
+  /**
+   * A neural tokenizer.
+   */
+  private val tokenizer: NeuralTokenizer = parsedArgs.tokenizerModelPath.let {
+    println("Loading tokenizer model from '$it'...")
+    NeuralTokenizer(NeuralTokenizerModel.load(FileInputStream(File(it))))
+  }
+
+  /**
+   * A neural LHR parser.
+   */
+  private val parser: LHRParser = parsedArgs.parserModelPath.let {
+    println("Loading parser model from '$it'...")
+    LHRParser(LHRModel.load(FileInputStream(File(it))))
+  }
+
+  /**
+   * A morphological analyzer.
+   */
+  private val analyzer = MorphologicalAnalyzer(dictionary = parsedArgs.morphoDictionaryPath.let {
+    println("Loading serialized dictionary from '$it'...")
+    MorphologyDictionary.load(FileInputStream(File(it)))
+  })
+
+  /**
+   * Summarize a text associating a salience score to each sentence that compose it.
+   *
+   * @param text a text
+   *
+   * @return a list of sentences associated to the related salience score
+   */
+  fun summarize(text: String): List<Pair<TkSentence, Double>> {
+
+    val timer = Timer()
+
+    println("Tokenizing sentences...")
+    val tkSentences: List<TkSentence> = this.tokenizer.tokenize(text).filter { it.tokens.isNotEmpty() }
+    println("${tkSentences.size} non-empty sentences found.")
+
+    println("Parsing sentences...")
+    val progress = ProgressIndicatorBar(tkSentences.size)
+    val parsedSentences: List<MorphoSynSentence> = tkSentences.map { sentence ->
+
+      @Suppress("UNCHECKED_CAST")
+      val parsingSentence = ParsingSentence(
+        tokens = sentence.tokens.mapIndexed { i, token ->
+          ParsingToken(id = i, form = token.form, position = token.position)
+        },
+        position = sentence.position,
+        labelerSelector = MorphoSelector,
+        morphoAnalysis = this.analyzer.analyze(sentence as RealSentence<RealToken>)
+      )
+
+      progress.tick()
+
+      this.parser.parse(parsingSentence)
+    }
+
+    println("Elapsed time: ${timer.formatElapsedTime()}")
+    timer.reset()
+
+    println("Summarizing...")
+    val salienceScores: List<Double> = Summarizer.getSalienceScores(parsedSentences)
+
+    println("Elapsed time: ${timer.formatElapsedTime()}")
+
+    return tkSentences.zip(salienceScores)
+  }
+}
---
+++ src/main/kotlin/com/kotlinnlp/summarizer/Summarizer.kt
@@ -0,0 +1,162 @@
+/* Copyright 2019-present The KotlinNLP Authors. All Rights Reserved.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, you can obtain one at http://mozilla.org/MPL/2.0/.
+ * ------------------------------------------------------------------*/
+
+package com.kotlinnlp.summarizer
+
+import ca.pfv.spmf.algorithms.frequentpatterns.lcm.AlgoLCM
+import ca.pfv.spmf.algorithms.frequentpatterns.lcm.Dataset
+import ca.pfv.spmf.patterns.itemset_array_integers_with_count.Itemset
+import com.kotlinnlp.linguisticdescription.morphology.morphologies.ContentWord
+import com.kotlinnlp.linguisticdescription.sentence.MorphoSynSentence
+import com.kotlinnlp.simplednn.simplemath.ndarray.Shape
+import com.kotlinnlp.simplednn.simplemath.ndarray.dense.DenseNDArray
+import com.kotlinnlp.simplednn.simplemath.ndarray.dense.DenseNDArrayFactory
+import com.kotlinnlp.utils.DictionarySet
+import kotlin.math.min
+
+/**
+ * Helper that calculates the salience scores of sentences that compose a text, with the purpose to build a summary.
+ *
+ * The algorithm is based on the LSA-itemset summarizer described in:
+ *   [Generazione automatica di riassunti di collezioni di documenti multilingua](http://webthesis.biblio.polito.it/id/eprint/6457)
+ * that uses the LCM algorithm to extract the frequent itemsets, described in:
+ *   [LCM ver. 2: Efficient Mining Algorithms for Frequent/Closed/Maximal Itemsets](http://ceur-ws.org/Vol-126/uno.pdf)
+ */
+object Summarizer {
+
+  /**
+   * The parameter given to the LCM as minimum support value (in percentage respect to the number of frequent itemsets
+   * collected in the whole text).
+   */
+  private const val MIN_LCM_SUPPORT = 0.01
+
+  /**
+   * The minimum number of lemmas that compose an ngram.
+   */
+  private const val MIN_NGRAM_SIZE = 2
+
+  /**
+   * The maximum number of lemmas that compose an ngram.
+   */
+  private const val MAX_NGRAM_SIZE = 4
+
+  /**
+   * @param sentences a list of sentences that compose a text
+   *
+   * @return the salience scores of the input sentences
+   */
+  fun getSalienceScores(sentences: List<MorphoSynSentence>): List<Double> {
+
+    require(sentences.isNotEmpty())
+
+    val sentencesOfLemmas: List<List<String>> = sentences.map { this.extractLemmas(it) }
+    val itemsetsMatrix: DenseNDArray = this.buildItemsetsMatrix(sentencesOfLemmas)
+    val (_, s, v) = itemsetsMatrix.sparseSVD()
+
+    // Note: singular values in S are sorted by descending value.
+    val singularValuesThreshold: Double = s[0] / 2
+    var relevantSingularValues = -1
+    while (s[++relevantSingularValues] >= singularValuesThreshold);
+
+    val rowScores: List<Double> = (0 until v.rows).map { k ->
+      (0 .. relevantSingularValues).sumByDouble { i ->
+        Math.pow(v[k, i], 2.0) * Math.pow(s[i], 2.0)
+      }
+    }
+    val maxRowScore: Double = rowScores.max()!!
+
+    return rowScores.map { it / maxRowScore }
+  }
+
+  /**
+   * @param sentence an input sentence
+   *
+   * @return the list of relevant lemmas that compose the sentence
+   */
+  private fun extractLemmas(sentence: MorphoSynSentence): List<String> =
+    sentence.tokens
+      .asSequence()
+      .map { it.flatMorphologies.first() }
+      .filter { it is ContentWord }
+      .map { it.lemma }
+      .toList()
+
+  /**
+   * @param sentencesOfLemmas a list of sentences (as lists of lemmas) that compose a text
+   *
+   * @return the matrix that represents the relation between sentences and frequent itemsets
+   */
+  private fun buildItemsetsMatrix(sentencesOfLemmas: List<List<String>>): DenseNDArray {
+
+    val sentencesOfInt: List<IntArray> = this.sentencesToItems(sentencesOfLemmas)
+
+    val dataset = Dataset(sentencesOfInt.filter { it.isNotEmpty() })
+    val frequentItemsets: List<Itemset> = AlgoLCM().runAlgorithm(MIN_LCM_SUPPORT, dataset, null).levels.flatten()
+    val itemsetsMatrix = DenseNDArrayFactory.zeros(Shape(frequentItemsets.size, sentencesOfInt.size))
+
+    sentencesOfInt.forEachIndexed { j, items ->
+      frequentItemsets.forEachIndexed { i, itemset ->
+        if (items.contains(itemset)) itemsetsMatrix[i, j] = 1.0
+      }
+    }
+
+    return itemsetsMatrix
+  }
+
+  /**
+   * Convert sentences of lemmas to ordered sequences of integer items.
+   * Each item refers to an ngram of consecutive lemmas, with a size between [MIN_NGRAM_SIZE] and [MAX_NGRAM_SIZE].
+   *
+   * @param sentencesOfLemmas a list of sentences (as lists of lemmas) that compose a text
+   *
+   * @return the sentences converted to ordered sequences of integer items
+   */
+  private fun sentencesToItems(sentencesOfLemmas: List<List<String>>): List<IntArray> {
+
+    val lemmasDictionary = DictionarySet<String>()
+    val ngramsDictionary = DictionarySet<List<Int>>()
+
+    return sentencesOfLemmas.map { sentence ->
+
+      val intSentence: List<Int> = sentence.map { lemma ->
+        lemmasDictionary.add(lemma)
+        lemmasDictionary.getId(lemma)!!
+      }
+      val sentenceItems: MutableSet<Int> = mutableSetOf()
+
+      (MIN_NGRAM_SIZE .. MAX_NGRAM_SIZE).forEach { ngramSize ->
+
+        val startIter: Int = min(ngramSize, sentence.size)
+
+        (startIter until sentence.size).forEach { endExclusive ->
+          val start: Int = endExclusive - ngramSize
+          val ngram: List<Int> = intSentence.subList(start, endExclusive)
+          ngramsDictionary.add(ngram)
+          sentenceItems.add(ngramsDictionary.getId(ngram)!!)
+        }
+      }
+
+      sentenceItems.sorted().toIntArray()
+    }
+  }
+
+  /**
+   * @param itemset an itemset
+   *
+   * @return true if this ordered int array contains the given items
+   */
+  private fun IntArray.contains(itemset: Itemset): Boolean {
+
+    val startIndex: Int = this.indexOf(itemset.items.first())
+
+    if (startIndex < 0) return false
+
+    val endIndex: Int = min(startIndex + itemset.size() - 1, this.lastIndex)
+
+    return this.sliceArray(startIndex .. endIndex).contentEquals(itemset.items)
+  }
+}
