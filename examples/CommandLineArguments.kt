ef22c0f73a5a802e662d6e4aba5487237ad37fde
Simone Cangialosi <sm.cangialosi@gmail.com>
2020-04-23 11:48:32 +0200

    Update headers

--- examples/CommandLineArguments.kt
+++ examples/CommandLineArguments.kt
@@ -1,4 +1,4 @@
-/* Copyright 2019-present KotlinNLP Authors. All Rights Reserved.
+/* Copyright 2019-present Simone Cangialosi. All Rights Reserved.
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
